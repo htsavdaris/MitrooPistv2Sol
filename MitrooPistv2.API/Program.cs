0ef8b06df7f208b6fdb6a8929b31f500d8166f25
Harry <hatsav@micro.com>
2021-11-02 04:04:18 +0200

    NLog

--- MitrooPistv2.API/Program.cs
+++ MitrooPistv2.API/Program.cs
@@ -6,6 +6,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using NLog.Web;
 
 namespace MitrooPistv2.API
 {
@@ -13,7 +14,25 @@ namespace MitrooPistv2.API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var config = new NLog.Config.LoggingConfiguration();
+            var logfile = new NLog.Targets.FileTarget("logfile") { FileName = "log.txt" };
+            config.AddRule(NLog.LogLevel.Debug, NLog.LogLevel.Fatal, logfile);
+            NLog.LogManager.Configuration = config;
+            var logger = NLog.LogManager.GetCurrentClassLogger();
+            try
+            {
+                logger.Debug("Application Starting Up");
+                CreateHostBuilder(args).Build().Run();
+            }
+            catch (Exception exception)
+            {
+                logger.Error(exception, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                NLog.LogManager.Shutdown();
+            }
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
