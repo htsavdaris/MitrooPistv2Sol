b426336ffb54daa637027ec8e45281a9ee6c3987
Harry Tsavdaris <hatsav@microsoft.com>
2021-12-10 15:37:24 +0200

    Swagger UI

--- MitrooPistv2.API/Startup.cs
+++ MitrooPistv2.API/Startup.cs
@@ -39,6 +39,8 @@ namespace MitrooPistv2.API
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Mitroo Pistopoiimenon Ektimiton API"));
             }
 
             app.UseRouting();
